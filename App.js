ab395ad79336acfc554284c413191d2294c859c2
hakimhassan97 <44751899+hakimhassan97@users.noreply.github.com>
2019-02-25 18:57:07 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Name : Mohamad Nurhakim Bin Hassan - 1621573</Text>
       </View>
     );
   }
